3266e2a781295b5307c71b550e8f7bc14ef80147
hermajor <hotdog451@gmail.com>
2017-02-01 17:53:03 +0300

    1.2 +

--- 2.php
+++ 2.php
@@ -4,13 +4,19 @@
  	for ($i = 1000; $i <= 9999; $i++){
 		$sumLastNum = 0;
 		$num = $i;
-		while ($num > 0){
+		if ($num > $n*1000){
+			echo "Дальше идут числа больше ". $n*1000;
+			break;
+		}
+		do {
 			$lastNum = $num % 10;
 			$sumLastNum += $lastNum;
 			$num = (int)($num / 10);
-		}
+			if ($lastNum > $n){
+				break;
+			}
+		} while ($num > 0);
 		if ($sumLastNum == $n){
 			echo $i."<br>";
 		}
 	}
-?>
