5de93f9557b4694cdaf5bf3e47317d1ea393e0e7
hermajor <hotdog451@gmail.com>
2017-02-03 15:49:03 +0300

    1.7

---
+++ 7.php
@@ -0,0 +1,20 @@
+<?php 
+
+$num = 56123456;
+$n = $num;
+
+while ($n > 0){
+	$lastNum = $n % 10;
+	$n = (int)($n / 10);
+	$m = $n;
+	
+	while ($m > 0){
+		$lastNum2 = $m % 10;
+		$m = (int)($m / 10);
+		
+		if ($lastNum == $lastNum2){
+			echo "В числе $num есть одинаковые цифры";
+			break 2;
+		}
+	}
+}
