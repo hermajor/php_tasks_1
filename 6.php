5975e988eb97bb0f9cc20328c5a81e45d27ca6e1
hermajor <hotdog451@gmail.com>
2017-02-06 11:40:35 +0300

    1.6 v3

--- 6.php
+++ 6.php
@@ -1,41 +1,25 @@
 <?php 
 
 for ($i = 1000; $i <= 9999; $i++){
-	$n = $i;	
-	$repeat0 = $repeat2 = $repeat3 = $repeat7 = 0;
+	$n = $i;
+	$sum = 0;
 
-	while ($n > 0){
+	while ($n > 0 && $i != 3333){	//(int)($n/10) != 0
 		$lastNum = $n % 10;
 		$n = (int)($n / 10);
 		
 		switch ($lastNum) {
 		case 0:
-			if ($repeat0 > 0){
-				continue 3;
-			}
-			$repeat0++;
-			break;
 		case 2:
-			if ($repeat2 > 0){
-				continue 3;
-			}
-			$repeat2++;
-			break;
 		case 3:
-			if ($repeat3 > 0){
-				continue 3;
-			}
-			$repeat3++;
-			break;
 		case 7:
-			if ($repeat7 > 0){
-				continue 3;
-			}
-			$repeat7++;
+			$sum += $lastNum;
 			break;
 		default:
 			continue 3;
 		}
 	}
-	echo $i.'<br>';
+	if ($sum == 12){
+		echo $i.'<br>';
+	}
 }
