f33e79ef36a11aa65536cbda1c0b8b8071421fbc
hermajor <hotdog451@gmail.com>
2017-02-06 09:18:50 +0300

    1.4 +++

--- 4.php
+++ 4.php
@@ -1,62 +1,28 @@
 <?php
-for ($i = 1000; $i <= 9999; $i++){
-	$n = $i;//4321
 
-	while ($n > 0){
-		$lastNum = $n % 10; //1
-		$n = (int)($n / 10); //432
-		$lastNum2 = $n % 10; //2
-		if ($lastNum <= $lastNum2){
-			//echo "$lastNum <= $lastNum2<br>";
-				while ($n > 0){
-					$lastNum = $n % 10; //2		3	4
-					$n = (int)($n / 10); //43	4	0
-					$lastNum2 = $n % 10; //3	4	0
-					if ($lastNum > $lastNum2){
-						continue 3;
-					}
-				}
+for ($i = 1000; $i <= 9999; $i +=2){
+	$n = $i;
+	$bo1 = true;
+	$bo2 = true;
+
+	while ((int)($n/10) != 0){
+		$lastNum = $n % 10; 
+		$n = (int)($n / 10); 
+		$lastNum2 = $n % 10; 
+		
+		if ($lastNum < $lastNum2){
+			$bo1 &= true;
+			$bo2 &= false;
+		} elseif ($lastNum > $lastNum2){
+			$bo1 &= false;
+			$bo2 &= true;
+		} else {
+			$bo1 &= false;
+			$bo2 &= false;
+		}
+		if ($bo1 == false && $bo2 == false){
 			continue 2;
 		}
 	}
-	
 	echo $i."<br>";
 }
-
-
-/*
-for ($i = 1000; $i <= 9999; $i += 2){
-	$n = $i;
-
-	while ($n > 0){
-		$lastNum = $n % 10;
-		$n = (int)($n / 10);
-		$lastNum2 = $n % 10;
-		if ($lastNum <= $lastNum2){
-			continue 2;
-		}
-	}
-	
-	echo $i."<br>";
-}
-
-/* <?php 
-
-for ($i = 1000; $i <= 9999; $i++){
-	$n = $i;
-	
-	if (($n % 2) > 0){
-		continue;
-	}
-
-	while ($n > 0){
-		$lastNum = $n % 10;
-		$n = (int)($n / 10);
-		$lastNum2 = $n % 10;
-		if ($lastNum <= $lastNum2){
-			continue 2;
-		}
-	}
-	
-	echo $i."<br>";
-} */
