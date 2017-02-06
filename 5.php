8904121393e2e11f169d0f201aebbabe4d0f8343
hermajor <hotdog451@gmail.com>
2017-02-06 10:29:57 +0300

    1.5 v2

--- 5.php
+++ 5.php
@@ -1,11 +1,12 @@
 <?php 
 
 $n = 123456789;
+$m = 0;
 
 	while ($n > 0){
 		$lastNum = $n % 10;
 		$n = (int)($n / 10);
-		$m .= $lastNum;
+		$m = $m * 10 + $lastNum;
 	}
-
-	echo (int)$m;
+	
+	var_dump ($m);
