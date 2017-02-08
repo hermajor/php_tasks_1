4f3b8e33c0b880747e252fe91e05bb2dc3d6ca05
hermajor <hotdog451@gmail.com>
2017-02-08 16:50:16 +0300

    1.11 v2

--- 11.php
+++ 11.php
@@ -3,18 +3,21 @@
 $m = 999;
 
 for ($i = 0; $i < $m; $i++){
-	$n = $i;
-	$sumOfNum = 0;
 	
-	while ($n > 0){
-		$lastNum = $n % 10;
-		$n = (int)($n / 10);
-		$sumOfNum += $lastNum;
-	}
-	
-	if ($sumOfNum == 0){// Избегаем деления на ноль
-		continue;
-	}elseif (($i % $sumOfNum) == 0){
-		echo $i.' = '.$sumOfNum.'<br>';
+	if ($i > 9){
+		$n = $i;
+		$sumOfNum = 0;
+		
+		while ($n > 0){
+			$lastNum = $n % 10;
+			$n = (int)($n / 10);
+			$sumOfNum += $lastNum;
+		}
+		
+		if (($i % $sumOfNum) == 0){
+			echo $i.'<br>';
+		}
+	} else {
+		echo $i.'<br>';
 	}
 }
