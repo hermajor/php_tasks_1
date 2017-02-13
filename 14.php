b984a277a5b1f99edcecb23653da9627df55dd11
hermajor <hotdog451@gmail.com>
2017-02-13 09:27:06 +0300

    1.14 v2

--- 14.php
+++ 14.php
@@ -13,9 +13,39 @@ for ($i = 3; $i <= 3571; $i+=2){
 	if (primeNum($i)){
 		$secondNum = $firstNum + 2;
 		
-		if (primeNum($secondNum)){
-			echo $firstNum.' и '.$secondNum.'<br>';
-			$counter++;
+		if (!primeNum($secondNum)){
+			$i += 2;
+			continue;
 		}
+	} else {
+		continue;
+	}
+	echo $firstNum.' и '.$secondNum.'<br>';
+	$counter++;
+}
+
+function primeNum($num){
+	$num = (float)$num;
+	if ($num % 2 == 0 || $num <= 0 || $num / (int)$num != 1){
+		return false;
+	}
+	if ($num == 2 || $num == 3){
+		return true;
+	}
+	
+	$sqrtNum = (int)sqrt($num) + 1;
+	
+	for ($i = 3; $i <= $sqrtNum; $i += 2){
+		$lastNum = $num % $i;
+		$condition = false;
+
+		if ($lastNum == 0){
+			return false;
+		}
+		$condition = true;
+	}
+
+	if ($condition == true){
+		return true;
 	}
 }
