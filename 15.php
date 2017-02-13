fab07a1874a201fea0e466fe3221e2a4dd18deed
hermajor <hotdog451@gmail.com>
2017-02-13 09:39:18 +0300

    1.15 v1

---
+++ 15.php
@@ -0,0 +1,24 @@
+<?php
+
+$n = 369;
+$counter = 2; //+2 уже за единицу и себя
+
+if ($n % 2 == 0){
+    // $n - четное число
+    $startI = 2;
+    $inc = 1;
+    $maxDiv = (int)($n / 2);
+} else {
+    // $n - НЕ четное число
+    $startI = 3;
+    $inc = 2;
+    $maxDiv = (int)($n / 3);
+}
+
+for ($i = $startI; $i <= $maxDiv; $i += $inc){
+    if ($n % $i == 0){
+        $counter++;
+    }
+}
+
+echo 'Число '.$n.' имеет '.$counter.' делителей';
