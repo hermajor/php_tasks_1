e24f38ec2541eb83d140fb4137c0d3b9a7576445
hermajor <hotdog451@gmail.com>
2017-02-14 09:26:56 +0300

    1.14 v4

--- 14.php
+++ 14.php
@@ -1,37 +1,54 @@
 <?php
 
 $display = 10;
-$iMin = 2; //продумать ввод четного и привести его к нечетному
-$iMax = 999; // пока не используется
+$start = 3;
+$finish = 333;
 
-$result = couplePrime($display, $iMin, $iMax);
-var_dump($result);
+$result = couplePrime($display, $start, $finish);
+//var_dump($result);
 
-function couplePrime($display, $iMin, $iMax){
-		$num = $iMin;
-		//$num = (float)$num;
-		if ($num % 2 == 0 || $num <= 0 || $num / (int)$num != 1){
-			//return false;
-			return couplePrime($display, $iMin+1, $iMax);//если начали с четного числа
-		}
-		if ($num == 3){
-			//return true;
-			echo $num.'<br>';
-			return couplePrime($display - 1, $iMin + 2, $iMax);
-		}
+function couplePrime($display, $start, $finish){
+	$num = (int)$start;
+	
+	if ($num % 2 == 0){
+		return couplePrime($display, $num + 1, $finish);
+	}
+	
+	if ($display <= 0 || $start >= $finish){
+		return 'The end';
+	}
+	
+	for ($i = $start; $i <= $finish; $i += 2){
+		$num = (int)$i;
 		
-		$sqrtNum = (int)sqrt($num) + 1;
-		
-		for ($i = 3; $i <= $sqrtNum; $i += 2){
-			$lastNum = $num % $i;
-			if ($lastNum == 0){
-				//return false;
-				return couplePrime($display, $iMin+2, $iMax);
+		if (primeNum($num)){
+			$secondNum = $num + 2;
+			
+			if (primeNum($secondNum)){
+				echo $num .' и '. $secondNum .' (счетчик: '. $display .')<br>';
+				return couplePrime($display - 1, $secondNum + 2, $finish);
 			}
+			return couplePrime($display, $secondNum, $finish);
 		}
-		if ($display > 0){
-			echo $num.'<br>';
-			return couplePrime($display - 1, $iMin + 2, $iMax);
+	}
+}
+
+function primeNum($num){
+	$num = (float)$num;
+	if ($num % 2 == 0 || $num <= 0 || $num / (int)$num != 1){
+		return false;
+	}
+	if ($num == 2 || $num == 3){
+		return true;
+	}
+	
+	$sqrtNum = (int)sqrt($num) + 1;
+	
+	for ($i = 3; $i <= $sqrtNum; $i += 2){
+		$lastNum = $num % $i;
+		if ($lastNum == 0){
+			return false;
 		}
-		return 'Конец';
+	}
+	return true;
 }
