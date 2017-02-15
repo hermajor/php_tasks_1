5e32bb310f5b579f554c0e830a6ad99ea34002f2
hermajor <hotdog451@gmail.com>
2017-02-15 12:02:21 +0300

    1.17 v1

---
+++ 17.php
@@ -0,0 +1,54 @@
+<?php
+
+$m = 1071;
+$n = 462;
+
+var_dump(gcd($m,$n));
+
+//Адовый перебор
+function gcd2($m, $n){
+	if($m < 0 || $n < 0 || ($m - (int)$m) != 0 || ($n - (int)$n) != 0){
+		return 'Необходимо вводить <b> натуральные </b> числа';
+	}
+	if ($m == $n || $n == 0){
+		return $m;
+	}
+	if ($m == 0){
+		return $n;
+	}
+	if ($m > $n){
+		$bigNum = $m;
+		$smallNum = $n;
+	}
+	if ($m < $n){
+		$bigNum = $n;
+		$smallNum = $m;
+	}
+	
+	for ($i = $smallNum; $i >= 1; $i--){
+		if($bigNum % $i == 0 && $smallNum % $i == 0){
+			return $i;
+		}
+	}
+}
+
+//Алгоритм Евклида
+function gcd($m, $n){
+	
+	function gcdIn($a, $b){
+		if($b == 0){
+			return $a;
+		}
+		$a = $a % $b;
+		return gcdIn($b, $a);
+	}
+	
+	if (($m == 0 || $n == 0) && ($m > 0 || $n > 0)){
+		return gcdIn($m, $n);
+	}
+	
+	if($m < 0 || $n < 0 || ($m - (int)$m) != 0 || ($n - (int)$n) != 0){
+		return 'Необходимо вводить <b> натуральные </b> числа';
+	}
+	return gcdIn($m, $n);
+}
