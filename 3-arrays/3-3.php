af6dc3e7dad16d1214226fa810170c4b3307683d
hermajor <hotdog451@gmail.com>
2017-02-17 09:43:14 +0300

    3.3 v2

--- 3-arrays/3-3.php
+++ 3-arrays/3-3.php
@@ -1,25 +1,27 @@
 <?php 
 
-$arr = array(1, 2, 3, 0, -5, 6, 7, -8, 9, 10, 11, 12, -13, 14, 15);
+$arr = array(1, 2, 3, 0, 5, -6, 7, 8, 9, -10, 11, -12, 13, 14, 15);
 
 $key = 0;
-$keyPlus = false;
+$flag = false;
 
 while ($arr[$key] || $arr[$key] === 0){
-	if ($arr[$key] > 0 && $keyPlus === false){
+	if ($arr[$key] > 0 && $flag == false){
+		$flag = true;
 		$keyPlus = $key;
-		$temp = $arr[$key];
+		$valuePlus = $arr[$key];
 	}
 	if ($arr[$key] < 0){
 		$keyMinus = $key;
-		
-		$arr[$keyPlus] = $arr[$keyMinus];
-		$arr[$keyMinus] = $temp;
 	}
 	$key++;
 }
-/*
+
+if($flag == true && $keyMinus){
+	$arr[$keyPlus] = $arr[$keyMinus];
+	$arr[$keyMinus] = $valuePlus;
+}
+
 echo '<pre>';
 print_r ($arr);
 echo '</pre>';
-*/
